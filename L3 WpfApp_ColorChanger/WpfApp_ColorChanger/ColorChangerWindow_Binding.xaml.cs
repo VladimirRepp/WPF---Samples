a83c4eb319d24d90cf31d380d9779afb5daeec34
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2026-09-07 18:18:44 +0700

    Add files via upload

---
+++ L3 WpfApp_ColorChanger/WpfApp_ColorChanger/ColorChangerWindow_Binding.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp_ColorChanger
+{
+    /// <summary>
+    /// Реализация через Binding
+    /// Здесь мы используем привязку (Binding) для связывания значений Slider с цветом, 
+    /// что позволяет автоматически обновлять цвет при изменении значений без необходимости обрабатывать события вручную
+    /// В идеальном варианте мы вообще убираем обработчик из Code-behind
+    /// А это переход к MVVM
+    /// </summary>
+    public partial class ColorChangerWindow_Binding : Window
+    {
+        public ColorChangerWindow_Binding()
+        {
+            InitializeComponent();
+
+            UpdateColor();
+        }
+
+        private void ColorSlider_ValueChanged(object sender,
+            RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateColor();
+        }
+
+        // C# отвечает только за изменение цвета
+        private void UpdateColor()
+        {
+            byte red = (byte)RedSlider.Value;
+            byte green = (byte)GreenSlider.Value;
+            byte blue = (byte)BlueSlider.Value;
+
+            Color color = Color.FromRgb(
+                red,
+                green,
+                blue);
+
+            ColorPreview.Background =
+                new SolidColorBrush(color);
+        }
+
+        private void NavigateButton_Click(object sender, RoutedEventArgs e)
+        {
+            ColorChangerWindow_ValueChanged valueChangedWindow = new ColorChangerWindow_ValueChanged();
+            valueChangedWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ L3 WpfApp_ColorChanger/WpfApp_ColorChanger/ColorChangerWindow_ValueChanged.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp_ColorChanger
+{
+    /// <summary>
+    /// Версия реализации через ValueChanged \
+    /// Здесь мы обрабатываем событие ValueChanged каждого Slider и обновляем цвет в обработчике.
+    /// </summary>
+    public partial class ColorChangerWindow_ValueChanged : Window
+    {
+        public ColorChangerWindow_ValueChanged()
+        {
+            InitializeComponent();
+
+            UpdateColor();
+        }
+      
+        // Все три Slider вызывают один обработчик
+        private void ColorSlider_ValueChanged(object sender,
+            RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateColor();
+        }
+
+        private void UpdateColor()
+        {
+            byte red = (byte)RedSlider.Value;
+            byte green = (byte)GreenSlider.Value;
+            byte blue = (byte)BlueSlider.Value;
+
+            Color color = Color.FromRgb(
+                red,
+                green,
+                blue);
+
+            // SolidColorBrush — это кисть, которая закрашивает область одним цветом
+            SolidColorBrush brush = new SolidColorBrush(color);
+            ColorPreview.Background = brush;
+
+            // Обновляем текст рядом со Slider
+            RedValueTextBlock.Text = red.ToString();
+            GreenValueTextBlock.Text = green.ToString();
+            BlueValueTextBlock.Text = blue.ToString();
+
+            RgbTextBlock.Text =
+                $"RGB: {red}, {green}, {blue}";
+        }
+
+        private void NavigateButton_Click(object sender, RoutedEventArgs e)
+        {
+            ColorChangerWindow_Binding bindingWindow = new ColorChangerWindow_Binding();
+            bindingWindow.Show();
+            this.Close();
+        }
+    }
+}
