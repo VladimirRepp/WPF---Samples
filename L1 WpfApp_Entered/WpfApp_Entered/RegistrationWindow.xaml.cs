11f1653d29e1548b549a3a58f244dadb73f1105c
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2026-09-07 18:15:35 +0700

    Add files via upload

---
+++ L1 WpfApp_Entered/WpfApp_Entered/RegistrationWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Entered
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterLabel_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void CityComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(CityComboBox.SelectedValue is ComboBoxItem selectedItem)
+            {
+                CityInfoTextBlock.Text = $"Вы выбрали город: {selectedItem.Content.ToString()}";
+            }
+            else
+            {
+                CityInfoTextBlock.Text = string.Empty;
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            if(string.IsNullOrEmpty(name) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Имя или не указан", "Ошибка", 
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            bool isBegore16 = ValueBefore16RadioButton.IsChecked == true;
+            bool isBetween16_22 = Value16_22RadioButton.IsChecked == true;
+            bool isAfter22 = ValueAfter22RadioButton.IsChecked == true;
+
+            if (isBegore16 == false && isBetween16_22 == false && isAfter22 == false)
+            {
+                MessageBox.Show("Не выбран возраст", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            bool isMale = MaleRadioButton.IsChecked == true;
+            bool isFemale = FemaleRadioButton.IsChecked == true;
+
+            if (isMale == false && isFemale == false)
+            {
+                MessageBox.Show("Не выбран пол", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            string city = CityInfoTextBlock.Text;
+            if (CityComboBox.SelectedValue is ComboBoxItem selectedItem)
+            {
+                city = selectedItem.Content.ToString();
+            }
+
+            if (string.IsNullOrEmpty(city))
+            {
+                MessageBox.Show("Не выбран город", "Ошибка",
+                   MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            bool isAgreement = AgreementCheckBox.IsChecked == true;
+
+            if (!isAgreement)
+            {
+                MessageBox.Show("Нет согласия", "Ошибка",
+                  MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            UserInfo.Name = name;
+            UserInfo.Password = password;
+            UserInfo.City = city;
+            UserInfo.IsAgreement = isAgreement;
+
+            if(isBegore16)
+                UserInfo.Age = EAgeState.Before16;
+            else if(isBetween16_22)
+                UserInfo.Age = EAgeState.Between16_22;
+            else if(isAfter22)
+                UserInfo.Age = EAgeState.After22;
+
+            if(isMale)
+                UserInfo.Gender = EGenderState.Male;
+            else if (isFemale)
+                UserInfo.Gender = EGenderState.Female;
+
+            MessageBox.Show($"Добро пожаловать, {UserInfo.Name}!",
+                "Успех", 
+                MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+    }
+}
---
+++ L1 WpfApp_Entered/WpfApp_Entered/UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Entered
+{
+    public static class UserInfo
+    {
+        public static string Name;
+        public static string Password;
+        public static string City;
+        public static bool IsAgreement;
+        public static EAgeState Age;
+        public static EGenderState Gender;
+    }
+
+    public enum EAgeState
+    {
+        Before16,
+        Between16_22,
+        After22
+    }
+
+    public enum EGenderState
+    {
+        Male,
+        Female
+    }
+}
