03dc5f8a68161f330bcf53c9b2e33bd469ad46bb
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2026-09-21 14:01:52 +0700

    Update Counter.cs

--- Wpf_Counter/Wpf_Counter/Models/Counter.cs
+++ Wpf_Counter/Wpf_Counter/Models/Counter.cs
@@ -3,41 +3,31 @@
 namespace Wpf_Counter.Models
 {
     /// <summary>
-    /// Модель - бизнес-логика
+    /// Модель - чистая бизнес-логика
     /// </summary>
-    public class Counter : INotifyPropertyChanged
+    public class Counter
     {
         private int _number;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
+    
         public int Count
         {
             get => _number;
-            set
-            {
-                _number = value;
-
-                PropertyChanged?.Invoke(
-                    this,
-                    new PropertyChangedEventArgs(
-                        nameof(Count)));
-            }
+            set => _number = value;
         }
-
+    
         public Counter()
         {
-            _number = 0;
+            _number = 0; 
         }
-
+         
         public Counter(int number)
         {
             _number = number;
         }
-
-        public void Increment() => Count = Count + 1;
-        public void Decrement() => Count = Count - 1;
-
-        public void ChangeTo(int number) => Count = Count + number;
+    
+        public void Increment() => _number++;
+        public void Decrement() => _number--;
+    
+        public void ChangeTo(int number) => _number += number;
     }
 }
